a41d04b97162b3380e4634a10d6b6ff7950d3107
joshIsCoding <yes_you_do@hotmail.com>
2020-01-01 18:54:24 +0000

    Add user validations specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_length_of(:password_digest).is_at_least(5) }
+    it { should allow_value(nil).for(:password)}
+  end
 end

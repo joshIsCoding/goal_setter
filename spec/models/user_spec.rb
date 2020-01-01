139640a7ee882020bea3dfed835ada5246ca42af
joshIsCoding <yes_you_do@hotmail.com>
2020-01-01 23:19:12 +0000

    Fix erroneous validation of length of password_digest; change to password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject(:user) { User.new(username: "test_user", password: "test_pass") }
+  let (:unique_user) { User.create!(username: "unique_user", password: "unique")}
   describe "validations" do
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username) }
     it { should validate_presence_of(:password_digest) }
-    it { should validate_length_of(:password_digest).is_at_least(5) }
+    it { should validate_length_of(:password).is_at_least(5) }
     it { should allow_value(nil).for(:password) }
   end
 

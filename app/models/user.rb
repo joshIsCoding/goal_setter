744c92c5ec55ad9ecbca1fece71f5445976c4ea0
joshIsCoding <yes_you_do@hotmail.com>
2020-01-01 23:42:36 +0000

    Add ::generate_session_token, #password= and the private callback #ensure_session_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,20 @@ class User < ApplicationRecord
    validates :username, presence: true, uniqueness: true
    validates :password_digest, presence: true
    validates :password, length: { minimum: 5, allow_nil: true } 
+   after_initialize :ensure_session_token
 
    attr_reader :password
+
+   def self.generate_session_token
+      SecureRandom::urlsafe_base64(16)
+   end
+   def password=(password)
+      @password = password
+      self.password_digest = BCrypt::Password.create(password)
+   end
+
+   private
+   def ensure_session_token
+      self.session_token ||= self.class.generate_session_token
+   end
 end

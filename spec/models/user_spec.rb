b5ddd7e970a90febe8ef895b5a1e62503a6426f9
joshIsCoding <yes_you_do@hotmail.com>
2020-01-01 23:41:51 +0000

    Remove redundent let instance of user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject(:user) { User.new(username: "test_user", password: "test_pass") }
-  let (:unique_user) { User.create!(username: "unique_user", password: "unique")}
+  
   describe "validations" do
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username) }

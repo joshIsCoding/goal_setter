524a73ab01902a590d0c2f8d32d98d80cf6d8069
joshIsCoding <yes_you_do@hotmail.com>
2020-01-01 23:25:32 +0000

    Add User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+   validates :username, presence: true, uniqueness: true
+   validates :password_digest, presence: true
+   validates :password, length: { minimum: 5, allow_nil: true } 
+
+   attr_reader :password
 end

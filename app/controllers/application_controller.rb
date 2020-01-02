7d129d64bc5457f8c060a45c6ef323057c2162d9
joshIsCoding <yes_you_do@hotmail.com>
2020-01-02 19:00:03 +0000

    Add login helper methods #current_user #is_logged_in? and #login_user!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+   helper_method :current_user
+   
+   def current_user
+      User.find_by(session_token: session[:session_token])
+   end
+
+   def is_logged_in?
+      current_user.exists?
+   end
+
+   def login_user!(user)
+      session[:session_token] = user.reset_session_token! unless user == current_user
+   end
 end

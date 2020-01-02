204270dc277fc2ca7bbc638917862e63391d7447
joshIsCoding <yes_you_do@hotmail.com>
2020-01-02 13:35:52 +0000

    Fill in system specs for user sign-up process

--- spec/system/user_auth_spec.rb
+++ spec/system/user_auth_spec.rb
@@ -2,17 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "User Authentication", type: :system do
    describe "User Registration" do
-      it "has a registration page"
+      it "has a registration page" do
+      visit(new_user_url)
+      expect(page).to have_content("Sign Up Here!")
+   end
 
       describe "Using the Registration Form" do
          context "with valid user credentials" do
-            
-            it "shows the user's username on the homepage"
+            it "shows the user's username on the homepage" do
+               fill_in('user_username', with: "fake_user")
+               fill_in('user_password', with: "wunderbar")
+               click_on('Sign Up')
+               expect(page).to have_content("fake_user")               
+            end
 
          end
 
          context "with invalid user credentials" do
-            it "Refreshes the registration form"
+            it "refreshes the registration form for another go" do
+               fill_in('user_username', with: "fake_user")
+               click_on('Sign Up')
+               expect(page).to have_content("Sign Up Here!")
+            end
          end
       end
    end

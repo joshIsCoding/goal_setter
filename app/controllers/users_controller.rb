3756884955e28f1ac2945d44bb7ce2bb7e33b500
joshIsCoding <yes_you_do@hotmail.com>
2020-01-02 20:21:05 +0000

    Fix syntax error in 'create' action redirection upon successful creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
       if @user.save
          login_user!(@user)
-         redirect_to :show
+         redirect_to user_url(@user)
       else
          flash.now[:errors] = @user.errors.full_messages
          render :new

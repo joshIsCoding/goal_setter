ea1dd3fc5c9d56f45dcb67502a2a8cc878770f24
joshIsCoding <yes_you_do@hotmail.com>
2020-01-02 00:54:37 +0000

    Create skeleton pending user-auth specs

---
+++ spec/system/user_auth_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "User Authentication", type: :system do
+   describe "User Registration" do
+      it "has a registration page"
+
+      describe "Using the Registration Form" do
+         context "with valid user credentials" do
+            
+            it "shows the user's username on the homepage"
+
+         end
+
+         context "with invalid user credentials" do
+            it "Refreshes the registration form"
+         end
+      end
+   end
+end

7c8ab148ec95ab7c32a0defe60dbab81e333d2b5
joshIsCoding <yes_you_do@hotmail.com>
2020-01-02 20:19:53 +0000

    Add 'create' session action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,19 @@ class SessionsController < ApplicationController
       render :new
    end
 
+   def create
+      @user = User.find_by_credentials(user_params[:username], user_params[:password])
+      if @user
+         login_user!(@user)
+         redirect_to user_url(@user)
+      else
+         flash.now[:errrors] = "Sorry, those credentials weren't valid."
+         render :new
+      end
+   end
+   
+   private
+   def user_params
+      params[:user]
+   end
 end

9a442a30bbf79e1e844e757a3392d92afc0a1537
joshIsCoding <yes_you_do@hotmail.com>
2020-01-02 14:58:31 +0000

    Add create action and user_params private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
       @user = User.new
       render :new
    end
+
+   def create
+      @user = User.new(user_params)
+      if @user.save
+         login_user!(@user)
+         redirect_to :show
+      else
+         flash.now[:errors] = @user.errors.full_messages
+         render :new
+      end
+   end
+
+   private
+   def user_params
+      params.require(:user).permit(:username, :password)
+   end
 end

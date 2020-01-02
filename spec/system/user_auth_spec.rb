940a67ca3ba6afc3a768e235c5e5499483fdb187
joshIsCoding <yes_you_do@hotmail.com>
2020-01-02 21:07:18 +0000

    Add existing user login section to specs and include explicit checks for error messages

--- spec/system/user_auth_spec.rb
+++ spec/system/user_auth_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "User Authentication", type: :system do
-   before(:each) { visit(new_user_path) }
-  
+      
    describe "User Registration" do
+      before(:each) { visit(new_user_path) }
+
       it "has a registration page" do
-      expect(page).to have_content("Sign Up Here!")
-   end
+         expect(page).to have_content("Sign Up Here!")
+      end
 
       describe "Using the Registration Form" do
          context "with valid user credentials" do
@@ -20,12 +21,47 @@ RSpec.describe "User Authentication", type: :system do
          end
 
          context "with invalid user credentials" do
-            it "refreshes the registration form for another go" do
+            it "refreshes the registration form with an error for another go" do
                fill_in("user_username", with: "fake_user")
                click_on("Sign Up")
+
                expect(page).to have_content("Sign Up Here!")
+               expect(page).to have_content("too short")
             end
          end
       end
    end
+
+   describe "User Login" do
+      let(:user) { User.create!(username: "faked_user", password: "geronimo")}
+      before(:each) { visit(new_session_path) }
+
+      it "has a login page" do
+         expect(page).to have_content("Login to Your Account")
+      end
+
+      describe "Using the Login Form" do
+         context "with valid user credentials" do
+            it "shows the user's username on the homepage" do
+               fill_in("user_username", with: user.username)
+               fill_in("user_password", with: user.password)
+               click_on("Sign In")
+               expect(page).to have_content(user.username)               
+            end
+
+         end
+
+         context "with invalid user credentials" do
+            it "refreshes the login page with an error for another go" do
+               fill_in("user_username", with: user.username)
+               click_on("Sign In")
+
+               expect(page).to have_content("Login to Your Account")
+               expect(page).to have_content("weren't valid")
+            end
+         end
+      end
+
+   end
+
 end

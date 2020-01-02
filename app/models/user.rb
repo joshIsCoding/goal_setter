7865126d16417e3337e034b634106859d97fc051
joshIsCoding <yes_you_do@hotmail.com>
2020-01-02 00:05:46 +0000

    Add #reset_session_token!

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,12 @@ class User < ApplicationRecord
       self.password_digest = BCrypt::Password.create(password)
    end
 
+   def reset_session_token!
+      self.session_token = self.class.generate_session_token
+      self.save!
+      self.session_token
+   end
+
    private
    def ensure_session_token
       self.session_token ||= self.class.generate_session_token

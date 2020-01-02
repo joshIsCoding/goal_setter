029cdb46de3d70a422d253a3eb2662daa9a7e8f7
joshIsCoding <yes_you_do@hotmail.com>
2020-01-02 14:58:02 +0000

    Add before command to ensure the users' registration page is visited for each test

--- spec/system/user_auth_spec.rb
+++ spec/system/user_auth_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "User Authentication", type: :system do
+   before(:each) { visit(new_user_path) }
+  
    describe "User Registration" do
       it "has a registration page" do
-      visit(new_user_path)
       expect(page).to have_content("Sign Up Here!")
    end
 
       describe "Using the Registration Form" do
          context "with valid user credentials" do
             it "shows the user's username on the homepage" do
-               fill_in('user_username', with: "fake_user")
-               fill_in('user_password', with: "wunderbar")
-               click_on('Sign Up')
+               fill_in("user_username", with: "fake_user")
+               fill_in("user_password", with: "wunderbar")
+               click_on("Sign Up")
                expect(page).to have_content("fake_user")               
             end
 
@@ -20,8 +21,8 @@ RSpec.describe "User Authentication", type: :system do
 
          context "with invalid user credentials" do
             it "refreshes the registration form for another go" do
-               fill_in('user_username', with: "fake_user")
-               click_on('Sign Up')
+               fill_in("user_username", with: "fake_user")
+               click_on("Sign Up")
                expect(page).to have_content("Sign Up Here!")
             end
          end

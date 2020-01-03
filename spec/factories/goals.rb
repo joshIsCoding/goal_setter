35b2d048e9b3d79b6e645500cf719ba7a3713612
joshIsCoding <yes_you_do@hotmail.com>
2020-01-03 13:32:16 +0000

    Initialize spec files for goals model

---
+++ spec/factories/goals.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :goal do
+    title { "MyString" }
+    details { "MyText" }
+    status { "MyString" }
+    public? { false }
+    user_id { "MyString" }
+  end
+end

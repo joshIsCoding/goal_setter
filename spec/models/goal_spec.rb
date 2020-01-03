b6dfbff40091afb45cfc15bf14aee9e21eb4c214
joshIsCoding <yes_you_do@hotmail.com>
2020-01-03 17:59:40 +0000

    Remove redundant presence_of validation test for user_id; this is covered by the belongs_to user associaiton spec

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Goal, type: :model do
     
 
     it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:user_id) }
     
     it do  
       should validate_inclusion_of(:status)

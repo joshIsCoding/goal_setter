f66b527f3615bb1d429b68ac5db123d60b8ce6c7
joshIsCoding <yes_you_do@hotmail.com>
2020-01-03 19:08:30 +0000

    Add goals resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create, :new, :show]
+  resources :users, only: [:create, :new, :show] do
+    resources :goals, only: [:index, :new]
+  end
   resource :session, only: [:create, :new, :destroy]
+  resources :goals, only: [:create, :update, :destroy]
 
 end

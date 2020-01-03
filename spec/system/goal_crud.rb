3faf98a8f7d2d34f71dad46dbcf9c2769cbb38b2
joshIsCoding <yes_you_do@hotmail.com>
2020-01-03 19:19:10 +0000

    Write pending goal creation specs

---
+++ spec/system/goal_crud.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Goal Creation, Updates and Deletion", type: :system do
+
+   describe "Goal Creation" do
+
+      it "let's a user create a new goal for themselves"
+
+      it "doesn't let users create goals for other users"
+
+      it "let's a user see all their goals"
+   end
+
+
+end

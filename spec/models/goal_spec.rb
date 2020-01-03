25e9eca6dc7553a1c66247db4bdfa2eac65f59f6
joshIsCoding <yes_you_do@hotmail.com>
2020-01-03 17:46:18 +0000

    Write validation and association specs

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Goal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:base_goal) { Goal.new(title: "Be Better!", user_id: 1) }
+  describe "validations" do
+    
+
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:user_id) }
+    
+    it do  
+      should validate_inclusion_of(:status)
+        .in_array ["Not Started", "In Progress", "Complete"] 
+    end
+
+    it { should validate_uniqueness_of(:title).scoped_to(:user_id)}
+  end
+
+  describe "associations" do
+    it { should belong_to(:user) }
+  end
+
 end

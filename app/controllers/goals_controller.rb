17bfd722e5822667124aba00cdbc11ca96107b1e
joshIsCoding <yes_you_do@hotmail.com>
2020-01-04 19:49:25 +0000

    Add 'create' action

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,4 +3,20 @@ class GoalsController < ApplicationController
       @goal = Goal.new
       render :new
    end
+
+   def create
+      @goal = Goal.new(goal_params)
+      @goal.user = current_user
+      if @goal.save
+         redirect_to user_url(current_user)
+      else
+         flash.now[:errors] = @goal.errors.full_messages
+         render :new
+      end
+   end
+
+   private
+   def goal_params
+      params.require(:goal).permit(:title, :details, :status, :public?)
+   end
 end

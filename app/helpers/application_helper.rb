c52dd8e0adb7cd74b69b4f92c7b9dcb6a080d128
joshIsCoding <yes_you_do@hotmail.com>
2020-01-04 17:29:33 +0000

    Add auth_token html-string helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+   def auth_token
+      auth_string = <<-HTML
+         <input type="hidden" 
+         name="authenticity_token" 
+         value= #{form_authenticity_token}" >
+      HTML
+      auth_string.html_safe
+   end
 end

f43ba977cb499ebf708f8a860a5c784b9af3091e
joshIsCoding <yes_you_do@hotmail.com>
2020-01-05 01:22:58 +0000

    Rework the Goal Privacy specs to correctly load the goals before the page in the goal visibility spec

--- spec/system/goal_crud.rb
+++ spec/system/goal_crud.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Goal Creation, Updates and Deletion", type: :system do
    let(:main_user) { User.create!(username: "Main", password: "main_pass")}
    let(:other_user) { User.create!(username: "Other", password: "other_pass")}
+
    before(:each) do
       visit(new_session_path)
       fill_in("user_username", with: main_user.username)
@@ -11,9 +12,6 @@ RSpec.describe "Goal Creation, Updates and Deletion", type: :system do
    end
 
    describe "Goal Creation" do
-      let(:goal_1) { Goal.create!(title: "Big Goal", details: "Smashing life", user: main_user, public?: false)}
-      let(:goal_2) { Goal.create!(title: "Mini Goal", details: "Smashing pumpkins", user: main_user)}
-
       it "lets a user create a new goal for themselves" do
          visit(new_user_goal_path(main_user))
          expect(page).to have_content("Set a new goal!")
@@ -29,15 +27,19 @@ RSpec.describe "Goal Creation, Updates and Deletion", type: :system do
          expect(page).to have_content("Write more specs!")
       end
 
-      it "doesn't let users create goals for other users" do
-         visit(new_user_goal_path(other_user))
-         expect(page).not_to have_content("Set!")
-      end
+      context "Goal Privacy" do
+         let!(:goal_1) { Goal.create!(title: "Big Goal", details: "Smashing life", user_id: main_user.id, public: false)}
+         let!(:goal_2) { Goal.create!(title: "Mini Goal", details: "Smashing pumpkins", user_id: main_user.id, public: true)}
+         it "doesn't let users create goals for other users" do
+            visit(new_user_goal_path(other_user))
+            expect(page).not_to have_content("Set!")
+         end
 
-      it "lets a user see all their goals, public and private" do
-         visit(user_path(main_user))
-         expect(page).to have_content(goal_1.title)
-         expect(page).to have_content(goal_2.title)         
+         it "lets a user see all their goals, public and private" do
+            visit(user_path(main_user))
+            expect(page).to have_content(goal_1.title)
+            expect(page).to have_content(goal_2.title)         
+         end
       end
    end
 

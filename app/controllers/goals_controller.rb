d97de5fc3c04cf0af49f78a70240354a7bd52559
joshIsCoding <yes_you_do@hotmail.com>
2020-01-05 19:59:10 +0000

    Add the new goal id anchor link to the 'create' action redirection url; change goal_params field permissions from 'public?' to public

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -8,7 +8,7 @@ class GoalsController < ApplicationController
       @goal = Goal.new(goal_params)
       @goal.user = current_user
       if @goal.save
-         redirect_to user_url(current_user)
+         redirect_to user_url(current_user, anchor: @goal.id)
       else
          flash.now[:errors] = @goal.errors.full_messages
          render :new
@@ -17,6 +17,6 @@ class GoalsController < ApplicationController
 
    private
    def goal_params
-      params.require(:goal).permit(:title, :details, :status, :public?)
+      params.require(:goal).permit(:title, :details, :status, :public)
    end
 end

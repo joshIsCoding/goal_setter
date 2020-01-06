676e85b96a1d51e082f99b897eaab9ec5e1ac151
joshIsCoding <yes_you_do@hotmail.com>
2020-01-06 00:32:16 +0000

    Add update and edit actions with associated  view

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -15,6 +15,26 @@ class GoalsController < ApplicationController
       end
    end
 
+   def edit
+      @goal = Goal.find_by_id(params[:id])
+      if @goal.user == current_user
+         render :edit
+      else
+         redirect_to goal_url(@goal)
+      end
+   end
+
+   def update
+      @goal = Goal.find_by_id(params[:id])
+      if @goal.update(goal_params)
+         redirect_to user_url(current_user, anchor: @goal.id)
+      else
+         flash.now[:errors] = @goal.errors.full_messages
+         render :edit
+      end
+   end
+
+
    private
    def goal_params
       params.require(:goal).permit(:title, :details, :status, :public)

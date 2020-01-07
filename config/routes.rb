a82bc33997fc199ae89deac914663a1d5c06fc8a
joshIsCoding <yes_you_do@hotmail.com>
2020-01-07 21:17:45 +0000

    Remove nested goals-index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :new, :show, :index] do
-    resources :goals, only: [:index, :new]
+    resources :goals, only: [:new]
   end
   resource :session, only: [:create, :new, :destroy]
   resources :goals, only: [:show, :create, :edit, :update, :destroy]

76d1b0521d3942e1043adf527c11d271e77bae8f
joshIsCoding <yes_you_do@hotmail.com>
2020-01-07 23:02:18 +0000

    Generate and run migration to create comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_04_232735) do
+ActiveRecord::Schema.define(version: 2020_01_07_224519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "contents", null: false
+    t.string "commentable_type"
+    t.bigint "commentable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+  end
 
   create_table "goals", force: :cascade do |t|
     t.string "title", null: false

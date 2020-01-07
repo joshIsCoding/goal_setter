db9ffe72041015e28c82636dd0137a49bcc6ee7d
joshIsCoding <yes_you_do@hotmail.com>
2020-01-07 21:04:59 +0000

    Add users-index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create, :new, :show] do
+  resources :users, only: [:create, :new, :show, :index] do
     resources :goals, only: [:index, :new]
   end
   resource :session, only: [:create, :new, :destroy]

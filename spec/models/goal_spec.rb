32843346ddb51192c084009981802d68acc62623
joshIsCoding <yes_you_do@hotmail.com>
2020-01-08 13:24:42 +0000

    Add has_many comments validation

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -3,20 +3,18 @@ require 'rails_helper'
 RSpec.describe Goal, type: :model do
   subject(:base_goal) { Goal.new(title: "Be Better!", user_id: 1) }
   describe "validations" do
-    
 
     it { should validate_presence_of(:title) }
-    
     it do  
       should validate_inclusion_of(:status)
         .in_array ["Not Started", "In Progress", "Complete"] 
     end
-
     it { should validate_uniqueness_of(:title).scoped_to(:user_id)}
   end
 
   describe "associations" do
     it { should belong_to(:user) }
+    it { should have_many(:comments)}
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe User, type: :model do
 
   describe "associations" do
     it { should have_many(:goals)}
+    it { should have_many(:comments)}
   end
 
   describe "model methods" do

f80128986c9e5243180bd87eadde8d566a59cc33
joshIsCoding <yes_you_do@hotmail.com>
2020-01-08 20:28:21 +0000

    Add skeleton comment creation, view and deletion specs

---
+++ spec/system/commenting_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Adding, Viewing and Deleting Comments", type: :system do
+
+   describe "Viewing Comments" do
+      it "shows user comments on the user page."
+
+      it "shows goal comments on the goal page."
+
+   end
+
+   describe "Adding Comments" do
+      it "lets a user comment on another user's profile"
+
+      it "lets a user comment on another user's goal"
+
+   end
+
+   describe "Deleting Comments" do
+      it "lets a user delete their own comments"
+
+      it "doesn't let a user delete other users' comments"
+   end
+
+
+end

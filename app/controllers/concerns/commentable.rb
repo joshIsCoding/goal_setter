f3e719afa63a8529742d5a1a01a40490f1d5f587
joshIsCoding <yes_you_do@hotmail.com>
2020-01-09 21:06:48 +0000

    Refactor sorted goal comments into Commentable#sorted_authored_comments

--- app/controllers/concerns/commentable.rb
+++ app/controllers/concerns/commentable.rb
@@ -7,4 +7,17 @@ module Commentable
       has_many :comments, as: :commentable
    end
 
+   def sorted_authored_comments
+      if self.comments
+         return self.comments.select(<<-SQL)
+            comments.*, 
+            users.username AS "author_name", 
+            users.id AS "author_id"
+         SQL
+         .joins(:author)
+         .order(:created_at)
+      else 
+         return []
+      end
+   end
 end

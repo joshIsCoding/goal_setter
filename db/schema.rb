f86f851f712bbe439e7ff95165b546cbf7dfbfc2
joshIsCoding <yes_you_do@hotmail.com>
2020-01-09 13:37:31 +0000

    Generate and run migration to add a comment author foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_224519) do
+ActiveRecord::Schema.define(version: 2020_01_09_132000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_01_07_224519) do
     t.bigint "commentable_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 

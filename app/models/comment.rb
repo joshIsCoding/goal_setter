4164b5b860535384653f64e88593d3fc697c2e1e
joshIsCoding <yes_you_do@hotmail.com>
2020-01-09 13:39:12 +0000

    Add belongs_to author (user) validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   validates :contents, presence: true, length: { maximum: 1000 }
   belongs_to :commentable, polymorphic: true
+  belongs_to( :author,
+    class_name: "User",
+    foreign_key: :author_id,
+    primary_key: :id,
+    dependent: :destroy)
 end

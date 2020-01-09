fc9d19bddf2a683b8a93c8b8f36fd8cf903ed670
joshIsCoding <yes_you_do@hotmail.com>
2020-01-09 22:19:46 +0000

    Add 'create' action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+   def create
+      @comment = Comment.new(comment_params.permit(:contents, :commentable_type, :commentable_id))
+      @comment.author = current_user
+
+      fallback = comment_params[:commentable_type] == "Goal" ? 
+      goal_url(comment_params[:commentable_id]) :
+      user_url(comment_params[:commentable_id])
+
+      flash[:errors] = @comment.errors.full_messages unless @comment.save         
+      redirect_back(fallback_location: fallback)
+   end
+
+   private
+   def comment_params
+      params.require(:comment)
+   end
+
 end

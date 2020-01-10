41a5d8a3e9686643c0772879c37e89303db910c9
joshIsCoding <yes_you_do@hotmail.com>
2020-01-10 13:34:08 +0000

    Add has_many authored_comments expectation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe User, type: :model do
   describe "associations" do
     it { should have_many(:goals)}
     it { should have_many(:comments)}
+    it { should have_many(:authored_comments)}
   end
 
   describe "model methods" do

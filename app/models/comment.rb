bcc6c615d5439a00b84988eb44e593a27d3e5fce
joshIsCoding <yes_you_do@hotmail.com>
2020-01-10 13:35:55 +0000

    Remove unwarranted destroy dependency from belongs to author association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,6 @@ class Comment < ApplicationRecord
   belongs_to( :author,
     class_name: "User",
     foreign_key: :author_id,
-    primary_key: :id,
-    dependent: :destroy)
+    primary_key: :id
+  )
 end

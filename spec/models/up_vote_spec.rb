5a8a908db791cba4d8d5a52662d73aff73f31e99
joshIsCoding <yes_you_do@hotmail.com>
2020-01-11 22:32:37 +0000

    Fix typo and add representative unique up-vote to validations test

--- spec/models/up_vote_spec.rb
+++ spec/models/up_vote_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe UpVote, type: :model do
   describe "Validations" do
-    it { should validate_uniqueness_of(:goal_id).scoped_to(:goal_id)}
+    let(:unique_up_vote) { Upvote.create!(user_id: 1, :goal_id: 1) }
+    it { should validate_uniqueness_of(:goal_id).scoped_to(:user_id)}
   end
 
   describe "Associations" do

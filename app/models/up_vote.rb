b5110b1ceac125e12ede49b0b079ae816db1c9b2
joshIsCoding <yes_you_do@hotmail.com>
2020-01-12 16:20:23 +0000

    Add custom user_has_up_votes_left validation

--- app/models/up_vote.rb
+++ app/models/up_vote.rb
@@ -1,5 +1,16 @@
 class UpVote < ApplicationRecord
-  validates :goal_id, uniqueness: { scope: :user_id }
+  UP_VOTE_LIMIT = 10
   belongs_to :user
   belongs_to :goal
+  validates :goal_id, uniqueness: { scope: :user_id }
+  validate :user_has_up_votes_left
+  
+
+  private
+  def user_has_up_votes_left
+    return unless self.user
+    unless UP_VOTE_LIMIT > self.user.up_votes.count
+      errors.add(:user, "has no upvotes remaining")
+    end
+  end
 end

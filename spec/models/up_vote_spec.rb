ea362489312c5223bd2f4785b97cb7109608b777
joshIsCoding <yes_you_do@hotmail.com>
2020-01-12 16:19:53 +0000

    Add validation specs to ensure that the user hasn't already exceeded their up-vote count

--- spec/models/up_vote_spec.rb
+++ spec/models/up_vote_spec.rb
@@ -2,8 +2,27 @@ require 'rails_helper'
 
 RSpec.describe UpVote, type: :model do
   describe "Validations" do
-    let(:unique_up_vote) { Upvote.create!(user_id: 1, :goal_id: 1) }
-    it { should validate_uniqueness_of(:goal_id).scoped_to(:user_id)}
+    let(:user) { User.create!(username: "the_user", password: "user_pass") }
+    let(:other_user) { User.create!(username: "other_user", password: "other_user_pass") }
+    
+    it do 
+      unique_up_vote = UpVote.create!(user_id: user.id, goal: Goal.create!(title: "unique goal", user: user))
+      should validate_uniqueness_of(:goal_id).scoped_to(:user_id)
+    end
+
+    it "does not permit more than 10 up-votes per user" do
+          10.times do |i|
+            UpVote.create!(
+              user: user, 
+              goal: Goal.create!(title: "Goal ##{i}", user: other_user)
+            )
+          end
+          last_up_vote = UpVote.new(
+              user: user,
+              goal: Goal.create!(title: "Goal #11", user: other_user)
+            )
+          expect(last_up_vote).not_to be_valid
+    end
   end
 
   describe "Associations" do

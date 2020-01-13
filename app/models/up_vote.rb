fdd34f29809835b9d9db3b786f652cc6bc43945c
joshIsCoding <yes_you_do@hotmail.com>
2020-01-13 12:58:27 +0000

    Add after create callback to decrement user's up_votes_left

--- app/models/up_vote.rb
+++ app/models/up_vote.rb
@@ -4,12 +4,17 @@ class UpVote < ApplicationRecord
   belongs_to :goal
   validates :goal_id, uniqueness: { scope: :user_id }
   validate :user_has_up_votes_left
+  after_create :decrement_user_up_votes!
   
-
   private
+
+  def decrement_user_up_votes!
+    self.user.decrement_up_votes_left!
+  end
+
   def user_has_up_votes_left
     return unless self.user
-    unless UP_VOTE_LIMIT > self.user.up_votes.count
+    unless self.user.up_votes_left > 0
       errors.add(:user, "has no upvotes remaining")
     end
   end

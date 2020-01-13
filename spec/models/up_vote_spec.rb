fb853e05d92c49db9ad8d1eb2f4f6b506c49e23b
joshIsCoding <yes_you_do@hotmail.com>
2020-01-13 12:47:13 +0000

    Add methods section for specs relating to creation and destroy callbacks

--- spec/models/up_vote_spec.rb
+++ spec/models/up_vote_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UpVote, type: :model do
-  describe "Validations" do
-    let(:user) { User.create!(username: "the_user", password: "user_pass") }
-    let(:other_user) { User.create!(username: "other_user", password: "other_user_pass") }
-    
+  let(:user) { User.create!(username: "the_user", password: "user_pass") }
+  let(:other_user) { User.create!(username: "other_user", password: "other_user_pass") }
+  
+  describe "Validations" do  
     it do 
       unique_up_vote = UpVote.create!(user_id: user.id, goal: Goal.create!(title: "unique goal", user: user))
       should validate_uniqueness_of(:goal_id).scoped_to(:user_id)
@@ -28,5 +28,16 @@ RSpec.describe UpVote, type: :model do
   describe "Associations" do
     it { should belong_to(:user) }
     it { should belong_to(:goal) }
+  end
+
+  describe "Methods" do
+    let(:goal) { Goal.new(title: "Method Goal", user: other_user)}
+    context "UpVote Creation" do
+      it "decrements the user's up_votes_left upon creation of an upvote" do
+        expect do
+          UpVote.create!(user: user, goal: goal)
+        end.to change{user.up_votes_left}.by(-1)
+      end
+    end
   end
 end

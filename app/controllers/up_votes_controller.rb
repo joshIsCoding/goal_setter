8342b8e64c415ce7be334ad630bc0f0a215b35cd
joshIsCoding <yes_you_do@hotmail.com>
2020-01-14 22:49:31 +0000

    Add create action

--- app/controllers/up_votes_controller.rb
+++ app/controllers/up_votes_controller.rb
@@ -1,6 +1,22 @@
-class ApplicationController < ApplicationController
+class UpVotesController < ApplicationController
 
    def create
+      up_vote_params = params[:up_vote]
+      target_goal = Goal.find_by_id(up_vote_params[:goal_id])
+      unless target_goal.user == current_user 
+         up_vote = UpVote.new(goal: target_goal, user: current_user)
+         if up_vote.save
+            flash[:notices] = ["Goal Upvoted!"]
+         else
+            flash[:errors] = up_vote.errors.full_messages
+         end
+      end
+      redirect_back(fallback_location: user_url(target_goal.user))
    end
 
+   private
+
+   def up_vote_params
+      params.require(:up_vote).permit(:goal_id)
+   end
 end

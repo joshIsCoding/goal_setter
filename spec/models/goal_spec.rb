64756d9d3904a46386323ca6e010e3f00133fb59
joshIsCoding <yes_you_do@hotmail.com>
2020-01-14 22:38:19 +0000

    Add method spec for #has_up_vote?(user)

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Goal, type: :model do
-  subject(:base_goal) { Goal.new(title: "Be Better!", user_id: 1) }
+  let(:user_1) { User.create!(username: "user_1", password: "1_password")}
+  subject(:base_goal) { Goal.new(title: "Be Better!", user: user_1) }
   describe "validations" do
 
     it { should validate_presence_of(:title) }
@@ -16,6 +17,22 @@ RSpec.describe Goal, type: :model do
     it { should belong_to(:user) }
     it { should have_many(:comments) }
     it { should have_many(:up_votes).dependent(:destroy) }
+  end
+
+  describe "methods" do
+    describe "#has_up_vote?(user)" do
+
+      it "returns false when the goal does not have an existing upvote by the provided user" do
+        expect(base_goal.has_up_vote?(user_1)).to be false
+      end
+      
+      it "returns true when the goal has an existing upvote by the provided user" do
+        user_2 = User.create!(username: "user_2", password: "2_password")
+        user_2_goal = Goal.create!(title: "Worthy Goal", user: user_2)
+        user_1_upvote = UpVote.create!(goal: user_2_goal, user: user_1) 
+        expect(user_2_goal.has_up_vote?(user_1)).to be true
+      end
+    end
   end
 
 end

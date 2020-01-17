9ade45b32f4ca8b51b11c41c25d16f0908304ea3
joshIsCoding <yes_you_do@hotmail.com>
2020-01-17 19:55:31 +0000

    Add Goal::leaderboard method specs

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Goal, type: :model do
-  let(:user_1) { User.create!(username: "user_1", password: "1_password")}
-  subject(:base_goal) { Goal.new(title: "Be Better!", user: user_1) }
+  let(:main_user) do 
+    User.create!(
+      username: "main_user", 
+      password: "main_password", 
+      up_votes_left: 10
+    )
+  end
+  subject!(:base_goal) { Goal.new(title: "Be Better!", user: main_user) }
   describe "validations" do
 
     it { should validate_presence_of(:title) }
@@ -20,17 +26,56 @@ RSpec.describe Goal, type: :model do
   end
 
   describe "methods" do
-    describe "#get_up_vote(user)" do
+    describe "::leaderboard" do
+      let(:users) do 
+        users = []
+        10.times do |i| 
+          users << User.create!(
+            username: "user_#{i}", 
+            password: "#{i}_password",
+            up_votes_left: 10
+          )
+        end 
+        users
+    end
+    let!(:upvoted_goals) do
+      goals = []
+      10.times do |i|
+        goals << curr_goal = Goal.create!(title: "Goal #{i}", user: users[i])
+        (i+1).times do |j| 
+          UpVote.create!(
+            goal: curr_goal,
+            user: (i == j) ? main_user : users[j]
+          )
+        end
+      end
+      goals
+    end
 
+      it "only returns 10 goals" do
+        expect(Goal.leaderboard.length).to be 10
+      end
+
+      it "returns goals in descending order of their cumulative received upvotes" do
+        expect(Goal.leaderboard).to match_array(upvoted_goals.reverse)
+      end
+
+      it "includes up_votes_count as a property of the goal records" do
+        expect(Goal.leaderboard.first.up_votes_count).to be 10
+      end
+
+    end
+    describe "#get_up_vote(user)" do
+      let(:user_2) { User.create!(username: "user_2", password: "password2") }
+      
       it "returns nil when the goal does not have an existing upvote by the provided user" do
-        expect(base_goal.get_up_vote(user_1)).to be nil
+        expect(base_goal.get_up_vote(user_2)).to be nil
       end
       
       it "returns the upvote by the provided user if it exists" do
-        user_2 = User.create!(username: "user_2", password: "2_password")
-        user_2_goal = Goal.create!(title: "Worthy Goal", user: user_2)
-        user_1_upvote = UpVote.create!(goal: user_2_goal, user: user_1) 
-        expect(user_2_goal.get_up_vote(user_1)).to eq(user_1_upvote)
+        user_2_goal = Goal.create!(title: "user_2_goal", user: user_2)
+        main_user_upvote = UpVote.create!(user: main_user, goal: user_2_goal)
+        expect(user_2_goal.get_up_vote(main_user)).to eq(main_user_upvote)
       end
     end
   end

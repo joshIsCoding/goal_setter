f99f60bce7d1c0ecebb8162c82310286f98db745
joshIsCoding <yes_you_do@hotmail.com>
2020-01-19 15:21:42 +0000

    Add ensure_session_token callback to set/ensure a token after initialisation

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,8 +1,14 @@
 class Session < ApplicationRecord
-  validates :session_token, presence: true, uniqueness: true
-  belongs_to :user
+   validates :session_token, presence: true, uniqueness: true
+   belongs_to :user
+   after_initialize :ensure_session_token
 
-  def self.generate_session_token
+   def self.generate_session_token
       SecureRandom::urlsafe_base64(16)
+   end
+
+   private
+   def ensure_session_token
+      self.session_token ||= self.class.generate_session_token
    end
 end

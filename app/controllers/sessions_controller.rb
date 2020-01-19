040a7bf71042ac86e8fb81992699ce054f564407
joshIsCoding <yes_you_do@hotmail.com>
2020-01-19 18:47:37 +0000

    Add index action and ensure this is skipped for the already_logged_in callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
    skip_before_action :ensure_login, except: [:destroy]
-   before_action :already_logged_in, except: [:destroy]
+   before_action :already_logged_in, except: [:index, :destroy]
    def new
-      render :new
+   end
+
+   def index
    end
 
    def create

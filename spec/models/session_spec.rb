b777d4face848a04b569eaeee58ac1d3dc5abb41
joshIsCoding <yes_you_do@hotmail.com>
2020-01-19 13:37:48 +0000

    Create unique session instance for uniqueness of token spec

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Session, type: :model do
   describe "Validations" do
+    subject(:unique_session) do
+      Session.create!(
+        user: User.create!(username: "test", password: "test_pass"), 
+        session_token: Session.generate_session_token
+      )
+    end
     it { should validate_presence_of(:session_token) }
     it { should validate_uniqueness_of(:session_token) }
   end

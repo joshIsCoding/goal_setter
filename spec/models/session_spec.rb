96266d43a07fecc0d06fcc358a6f3121b5cdcbc8
joshIsCoding <yes_you_do@hotmail.com>
2020-01-19 15:20:22 +0000

    Add ensure_session_token callback specs

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Session, type: :model do
+  let(:user) { User.create!(username: "test", password: "test_pass") }
+  
   describe "Validations" do
     subject(:unique_session) do
       Session.create!(
-        user: User.create!(username: "test", password: "test_pass"), 
+        user: user, 
         session_token: Session.generate_session_token
       )
     end
@@ -14,5 +16,17 @@ RSpec.describe Session, type: :model do
 
   describe "Associations" do
     it { should belong_to(:user) }
+  end
+
+  describe "Methods" do
+    
+    describe("Session token callback") do
+      it "should ensure a session_token is set after initialize" do
+        new_session = Session.new(user: user)
+        expect(new_session.session_token).not_to be nil
+        expect{new_session.save!}.not_to raise_error
+      end
+    end
+    
   end
 end

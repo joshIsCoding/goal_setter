a20bfb57b5a02da1210b541f0d53fdc6795bdc2f
joshIsCoding <yes_you_do@hotmail.com>
2020-01-20 16:39:15 +0000

    Add callback to parse the user_agent string and set browser and device values accordingly

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,7 +1,32 @@
 class Session < ApplicationRecord
-   validates :session_token, presence: true, uniqueness: true
+   EASY_BROWSERS = {
+      ["Firefox\/", "Seamonkey\/"] => "Firefox",
+      ["Seamonkey\/"] => "Seamonkey",
+      ["Chromium\/"] => "Chromium",
+      ["Safari\/", "Chrom(e|ium)\/"] => "Safari",
+      ["OPR\/"] => "Opera",
+      ["Opera\/"] => "Opera",
+      ["Trident/7.0; \.\*rv:"] => "Internet Explorer",
+      ["; MSIE "] => "Internet Explorer"
+   }.freeze
+   
+   TRICKY_BROWSERS = {
+      ["Chrome\/"] => "Chrome"
+   }.freeze
+
+   DEVICES = [
+      "iPhone",
+      "iPad",
+      "Windows Phone",
+      "Linux",
+      "Android",
+      "BlackBerry" #though this would seem unlikely
+   ].freeze
+   after_initialize :ensure_session_token   
+   validates :session_token, presence: true, uniqueness: true, on: :create
+   
+   before_create :parse_user_agent
    belongs_to :user
-   after_initialize :ensure_session_token
 
    def self.generate_session_token
       SecureRandom::urlsafe_base64(16)
@@ -10,5 +35,47 @@ class Session < ApplicationRecord
    private
    def ensure_session_token
       self.session_token ||= self.class.generate_session_token
+   end
+
+   def parse_user_agent
+      if self.user_agent
+         set_browser_from_user_agent
+         set_device_from_user_agent
+      end
+   end
+
+   def set_browser_from_user_agent
+      browser_conditions = [EASY_BROWSERS, TRICKY_BROWSERS] # easy browsers must
+      # be ruled out first as the tricky ones overlap
+      browser_conditions.each do |browser_condition_list|
+         browser_condition_list.each do |condition, browser|
+            next unless self.user_agent.match?(browser_version_regex(condition.first))
+            if condition.length == 2
+               next if self.user_agent.match?(browser_version_regex(condition.last))
+            end
+            self.browser = browser
+            return
+         end
+      end
+      self.browser = "Unrecognised"
+   end
+
+   def set_device_from_user_agent
+      DEVICES.each do |device| 
+         if self.user_agent.include?(device)
+            self.device = device
+            return
+         end 
+      end
+            
+      if self.user_agent.include?("Mobi")
+         self.device = "Mobile Device"
+      else
+         self.device = "Desktop"
+      end
+   end
+
+   def browser_version_regex(insert)
+      /#{insert}\d+\.\d+/
    end
 end

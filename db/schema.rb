84d8d3ac01036a4b564cf403c3b7e8e0e11ab084
joshIsCoding <yes_you_do@hotmail.com>
2020-02-10 19:16:57 +0000

    Add null:false constraint to categories.name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_133033) do
+ActiveRecord::Schema.define(version: 2020_02_10_191503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.bigint "goals_id", null: false
     t.index ["goals_id"], name: "index_categories_on_goals_id"
   end

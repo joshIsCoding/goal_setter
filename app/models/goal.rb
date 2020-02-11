6fcfc4ed08c90b26466a25b95aa3cd732a66eb53
joshIsCoding <yes_you_do@hotmail.com>
2020-02-11 21:16:33 +0000

    Add ::set_public & ::with_up_votes_count scopes; refactor ::leaderboard

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -9,16 +9,21 @@ class Goal < ApplicationRecord
    validates :title, uniqueness: { scope: :user_id },  presence: true
    validates :status, inclusion: { in: STATUSES }
 
+   scope :set_public, -> { where(public: true) }
+   scope :with_up_votes_count, -> do
+      select("goals.*, COUNT(up_votes.id) AS \"up_votes_count\"")
+      .left_outer_joins(:up_votes)
+      .group(:id)
+   end
+
    belongs_to :user
    has_many :up_votes, dependent: :destroy
    has_and_belongs_to_many :categories
 
    def self.leaderboard
-      self.select("goals.*, COUNT(up_votes.id) AS \"up_votes_count\"")
-      .left_outer_joins(:up_votes)
-      .group(:id)
-      .order(up_votes_count: :desc)
-      .limit(10)
+      self.all.set_public
+      .with_up_votes_count
+      .order(up_votes_count: :desc).limit(10)
    end
 
    def get_up_vote(user)

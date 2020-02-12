ee365892e443edf36a1fec3bec583a07365effee
joshIsCoding <yes_you_do@hotmail.com>
2020-02-12 13:13:49 +0000

    Update #type to #event_type

--- app/models/key_event.rb
+++ app/models/key_event.rb
@@ -1,5 +1,5 @@
 class KeyEvent < ApplicationRecord
-  validates_inclusion_of :type, in: ["new", "update"]
+  validates_inclusion_of :event_type, in: ["new", "update"]
   belongs_to :eventable, polymorphic: true
   belongs_to :instigator, class_name: "User"
 end

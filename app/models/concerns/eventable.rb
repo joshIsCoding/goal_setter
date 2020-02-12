a0c977416e9ffec3742d37593c0ed5960feaec93
joshIsCoding <yes_you_do@hotmail.com>
2020-02-12 13:23:33 +0000

    Create Eventable module with event generation callback

---
+++ app/models/concerns/eventable.rb
@@ -0,0 +1,24 @@
+require 'active_support/concern'
+
+module Eventable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :key_events, as: :eventable
+  end
+
+  def asset_owner
+    self.user
+  end
+
+  private
+
+  def generate_event(event_type)
+    self.key_events << KeyEvent.create(
+      eventable: self,
+      event_type: event_type,
+      instigator: self.asset_owner
+    )
+  end
+end
+

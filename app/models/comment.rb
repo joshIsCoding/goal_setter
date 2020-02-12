9dea31b3d9e03e3568aa4075e7523744e1501273
joshIsCoding <yes_you_do@hotmail.com>
2020-02-12 23:34:31 +0000

    Include Eventable, add generate_event callback and redefine inherited #asset_owner

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,16 @@
 class Comment < ApplicationRecord
+  include Eventable
+
   validates :contents, presence: true, length: { maximum: 1000 }
+  after_create { generate_event("new") }
   belongs_to :commentable, polymorphic: true
   belongs_to( :author,
     class_name: "User",
     foreign_key: :author_id,
     primary_key: :id
   )
+
+  def asset_owner #overwrites Eventable method
+    self.author    
+  end
 end

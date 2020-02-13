f5b348d8ed7db4aad4a264a4f5dacaf1cf41c0bc
joshIsCoding <yes_you_do@hotmail.com>
2020-02-13 19:44:31 +0000

    Add notification generation functionality for new upvote events

--- app/models/key_event.rb
+++ app/models/key_event.rb
@@ -2,5 +2,26 @@ class KeyEvent < ApplicationRecord
   validates_inclusion_of :event_type, in: ["new", "update"]
   belongs_to :eventable, polymorphic: true
   belongs_to :instigator, class_name: "User"
-  has_many :notifications
+  has_many :notifications, dependent: :destroy
+  after_create :generate_notifications
+
+  private
+
+  def notification_recipients
+    recipients = []
+    case self.eventable_type
+    when "UpVote"
+      recipients << self.eventable.goal.user
+    end
+    return recipients
+  end
+
+  def generate_notifications
+    Notification.transaction do
+      notification_recipients.each do |recipient| 
+        Notification.create(user: recipient, key_event: self)
+      end
+    end
+    self.notifications_generated = true    
+  end
 end

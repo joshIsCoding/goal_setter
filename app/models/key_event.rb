18f0c58ea43775ce4177ff60c773b1d8d6af108a
joshIsCoding <yes_you_do@hotmail.com>
2020-02-13 14:10:51 +0000

    Add notifications association

--- app/models/key_event.rb
+++ app/models/key_event.rb
@@ -2,4 +2,5 @@ class KeyEvent < ApplicationRecord
   validates_inclusion_of :event_type, in: ["new", "update"]
   belongs_to :eventable, polymorphic: true
   belongs_to :instigator, class_name: "User"
+  has_many :notifications
 end

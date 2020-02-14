f12cc5aa360cbf17fac4378925b1951f6a33fdac
joshIsCoding <yes_you_do@hotmail.com>
2020-02-14 17:56:50 +0000

    Add creation-time sort-order scope to #notifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
       primary_key: :id,
       dependent: :destroy
   )
-  has_many :notifications, dependent: :destroy
+  has_many :notifications, -> { order "created_at DESC" }, dependent: :destroy
 
    def self.generate_session_token
       SecureRandom::urlsafe_base64(16)

9458b148c0cb80be5d60ddfb4a47caab0e7b870d
joshIsCoding <yes_you_do@hotmail.com>
2020-02-14 17:55:48 +0000

    Add unseen and menu scopes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,7 @@ class Notification < ApplicationRecord
   validates :key_event_id, uniqueness: { scope: :user_id }
   belongs_to :user, inverse_of: :notifications
   belongs_to :key_event, inverse_of: :notifications
+
+  scope :unseen, -> { where(seen: false) }
+  scope :menu, -> { limit(10) }
 end

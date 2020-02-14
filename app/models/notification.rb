ff3ea5131cca2211e72a49aabb0e977c8d87560e
joshIsCoding <yes_you_do@hotmail.com>
2020-02-14 21:39:13 +0000

    Add :upvote, :goal and :comment associations to replace :source

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,9 @@ class Notification < ApplicationRecord
   validates :key_event_id, uniqueness: { scope: :user_id }
   belongs_to :user, inverse_of: :notifications
   belongs_to :key_event, inverse_of: :notifications
-  has_one :source, through: :key_event, source: :eventable
+  has_one :upvote, through: :key_event, source: :eventable, source_type: "UpVote"
+  has_one :goal, through: :key_event, source: :eventable, source_type: "Goal"
+  has_one :comment, through: :key_event, source: :eventable, source_type: "Comment"
 
   scope :unseen, -> { where(seen: false) }
   scope :menu, -> { limit(10) }

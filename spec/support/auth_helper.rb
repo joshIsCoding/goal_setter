00984a93d41b42ae6e14a049b95faf0ad92b6d81
joshIsCoding <yes_you_do@hotmail.com>
2020-02-14 18:31:39 +0000

    Add user auth helper methods

---
+++ spec/support/auth_helper.rb
@@ -0,0 +1,27 @@
+module AuthHelper
+
+  def register(username, pass_override = nil)
+    visit register_path
+    within 'form.user' do
+      fill_in 'user_username', with: username 
+      fill_in(
+        'user_password', 
+        with: pass_override ? pass_override : "password"
+      )
+      click_button 'SIGN UP'
+    end
+  end
+  
+  def login(user, pass_override = nil)
+    visit login_path
+    within 'form.user' do
+      fill_in 'user_username', with: user.username 
+      fill_in(
+        'user_password', 
+        with: pass_override ? pass_override : user.password
+      )
+      click_button 'SIGN IN'
+    end
+  end
+
+end

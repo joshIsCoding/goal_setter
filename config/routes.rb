be31a2748f19f9ea0f4a6327bb39cb3f988f0e8e
joshIsCoding <yes_you_do@hotmail.com>
2020-02-14 18:34:35 +0000

    Add notification routes; rename register and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create, :new, :show, :index] do
+  resources :users, only: [:create, :show, :index] do
     resources :goals, only: [:new]
     resources :sessions, only: [:index]
+    resources :notifications, only: [:index]
   end
-  resources :sessions, only: [:create, :new, :destroy]
+  get "register", to: "users#new"
+  get "login", to: "sessions#new"
+
+  resources :sessions, only: [:create, :destroy]
   resources :goals, only: [:show, :index, :create, :edit, :update, :destroy]
   resources :comments, only: [:create, :destroy]
   resources :up_votes, only: [:create, :destroy]
-
-  root 'welcome#index'
+  resources :notifications, only: [:update]  
 end

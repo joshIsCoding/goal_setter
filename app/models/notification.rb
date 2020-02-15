3fc8e6604c5d9ab3a3df20fe905208e2a53f2900
joshIsCoding <yes_you_do@hotmail.com>
2020-02-15 01:09:53 +0000

    Add :instigator association

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,7 @@ class Notification < ApplicationRecord
   validates :key_event_id, uniqueness: { scope: :user_id }
   belongs_to :user, inverse_of: :notifications
   belongs_to :key_event, inverse_of: :notifications
+  has_one :instigator, through: :key_event, source: :instigator
   has_one :upvote, through: :key_event, source: :eventable, source_type: "UpVote"
   has_one :goal, through: :key_event, source: :eventable, source_type: "Goal"
   has_one :comment, through: :key_event, source: :eventable, source_type: "Comment"

baf55c2388ea74d726ff98fa259c768a256e2e83
joshIsCoding <yes_you_do@hotmail.com>
2020-02-16 00:09:36 +0000

    Tweak messages to remove trailing punctuation

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,14 +1,14 @@
 module NotificationsHelper
   NOTIFICATION_MESSAGES = {
-    "new-Comment" => " commented after you.",
-    "new-UpVote"  => " just upvoted your goal!",
-    "update-Goal" => " has updated their goal."
+    "new-Comment" => " commented after you",
+    "new-UpVote"  => " just upvoted your goal",
+    "update-Goal" => " has updated their goal"
   }.freeze
 
   def parse_notification(notification)
     key_event = notification.key_event
     lookup = key_event.event_type + "-" + key_event.eventable_type
-    key_event.instigator.username.to_s + NOTIFICATION_MESSAGES[lookup]
+    NOTIFICATION_MESSAGES[lookup]
   end
 
   def notification_count

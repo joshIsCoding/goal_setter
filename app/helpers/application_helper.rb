800958093dc4fa2274ee85684bc02f10b1a47827
joshIsCoding <yes_you_do@hotmail.com>
2020-02-18 23:30:49 +0000

    Add ::UP_VOTE_ICONS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+   UP_VOTE_ICONS = {
+      "up" => "<i aria=hidden=\"true\" class=\"fas fa-chevron-circle-up\" title=\"Up Vote\"></i>",
+      "down" => "<i aria=hidden=\"true\" class=\"fas fa-chevron-circle-down\" title=\"Remove Up Vote\"></i>"
+   }
+   
    def auth_token
       auth_string = "<input type=\"hidden\" "
       auth_string << "name=\"authenticity_token\" "

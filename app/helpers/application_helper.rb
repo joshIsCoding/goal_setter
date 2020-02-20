a59bc93d781690629f225114d901e37d4caec3f3
joshIsCoding <yes_you_do@hotmail.com>
2020-02-20 15:15:21 +0000

    Add further action icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,14 @@
 module ApplicationHelper
    UP_VOTE_ICONS = {
-      "up" => "<i aria=hidden=\"true\" class=\"fas fa-chevron-circle-up\" title=\"Up Vote\"></i>",
-      "down" => "<i aria=hidden=\"true\" class=\"fas fa-chevron-circle-down\" title=\"Remove Up Vote\"></i>"
+      "up" => "<i aria-hidden=\"true\" class=\"fas fa-chevron-circle-up\" title=\"Up Vote\"></i>",
+      "down" => "<i aria-hidden=\"true\" class=\"fas fa-chevron-circle-down\" title=\"Remove Up Vote\"></i>"
+   }
+
+   ACTION_ICONS = {
+      "edit" => "<i aria-hidden=\"true\" class=\"fas fa-pen-square\" title=\"Edit\"></i><span class=\"sr-only\">Edit goal</span>",
+      "delete" => "<i aria-hidden=\"true\" class=\"fas fa-minus-square\" title=\"Delete\"></i><span class=\"sr-only\">Delete goal</span>",
+      "complete" => "<i aria-hidden=\"true\" class=\"fas fa-check-square\" title=\"Mark as complete\"></i><span class=\"sr-only\">Mark goal as complete</span>",
+      "trash" => "<i aria-hidden=\"true\" class=\"fas fa-trash-alt\" title=\"Delete comment\"></i><span class=\"sr-only\">Delete comment</span>"
    }
    
    def auth_token

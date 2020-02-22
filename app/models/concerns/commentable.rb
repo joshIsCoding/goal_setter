f4d75d3a43521272234082378e7ea315f8a2b33e
joshIsCoding <yes_you_do@hotmail.com>
2020-02-22 16:06:29 +0000

    Remove sorting from #sorted_authored_comments; rename accordingly

--- app/models/concerns/commentable.rb
+++ app/models/concerns/commentable.rb
@@ -7,17 +7,12 @@ module Commentable
       has_many :comments, as: :commentable
    end
 
-   def sorted_authored_comments
-      if self.comments
-         return self.comments.select(<<-SQL)
-            comments.*, 
-            users.username AS "author_name", 
-            users.id AS "author_id"
-         SQL
-         .joins(:author)
-         .order(:created_at)
-      else 
-         return []
-      end
+   def authored_comments
+      self.comments.select(<<-SQL)
+         comments.*, 
+         users.username AS "author_name", 
+         users.id AS "author_id"
+      SQL
+      .joins(:author)
    end
 end
